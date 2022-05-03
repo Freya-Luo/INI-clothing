a625d190e22af6e610a25dc5785f446496eec1fe
Freya-Luo <y233luo@gmail.com>
2022-05-03 11:23:00 +0200

    add: redux-persist and redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,24 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { rootReducer } from './root-reducer';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from "redux";
+import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
-const middleWares = [logger];
+// log redux state changes when in development mode
+const middleWares = process.env.NODE_ENV === "development" ? [logger] : [];
 
-export const store = createStore(rootReducer, undefined, compose(applyMiddleware(...middleWares)));
+// use redux devetools Chrome extension in the development mode
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"], // exclude some reducers to not persist them
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composeEnhancer(applyMiddleware(...middleWares)));
+
+export const persistor = persistStore(store);
