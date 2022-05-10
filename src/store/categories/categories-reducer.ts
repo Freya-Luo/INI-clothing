89f095370d50d09c0167b82feb94cd557023ce29
Freya-Luo <y233luo@gmail.com>
2022-05-09 22:44:59 -0400

    add: matcher to the action creators

--- src/store/categories/categories-reducer.ts
+++ src/store/categories/categories-reducer.ts
@@ -13,6 +13,16 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
   error: null,
 };
 
+/**
+ * Any actions (e.g., even init actions) that fire will pass through all reducers,
+ * which is how redux handles the action dispatch.
+ * Again, the way that redux determines that this does not need to be re-rendered is
+ * through the default return.
+ *
+ * @param state current category state passed to this reducer
+ * @param action action passed through
+ * @returns new category state modified based on the action
+ */
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as CategoriesAction) => {
   switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
@@ -24,7 +34,7 @@ export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} 
       return { ...state, loading: false, categories: action.payload };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAIL:
       return { ...state, loading: false, error: action.payload };
-    default:
+    default: // using matcher to give a type guard, since TS (static compiler) not detect the default case
       return state;
   }
 };
--- src/utils/reducer/reducer.ts
+++ src/utils/reducer/reducer.ts
@@ -1,5 +1,34 @@
 import { AnyAction } from "redux";
 
+/** Match extension definition.
+ * A type prediate: a func that verifies if a specific argument that it receives
+ * is a narrower/more specific type.
+ *
+ * AC: Action Creator; ReturnType<AC> gets the Action/ActionPayload
+ */
+type Match<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"]; // get the "type" field in the return value of createAction()
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+// overload signatures for matcher
+// actionCreator with no arguments : here, also specify "type" field must be included
+export function matcher<AC extends () => AnyAction & { type: string }>(actionCreator: AC): Match<AC>;
+// actionCreator with some arguments
+export function matcher<AC extends (...args: any[]) => AnyAction & { type: string }>(actionCreator: AC): Match<AC>;
+// implementation signatures
+export function matcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
+/** ----------- Action type definition. ------------ */
+// define the action type
 export type Action<T> = {
   type: T;
 };
@@ -16,3 +45,8 @@ export function createAction<T extends string>(type: T, payload: void): Action<T
 export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
 }
+
+/**
+ * 1) '&' intersection keyword: intersection type is a joining of different types
+ * 2) ReturnType<Func>: get the return type of a function Func
+ */
